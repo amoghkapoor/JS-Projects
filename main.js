ebd759a727ff11952e8794a7377e2ed67a6e86e4
amoghkapoor <72312945+amoghkapoor@users.noreply.github.com>
2021-06-09 22:47:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+anime({
+    targets: '#yt-count',
+    textContent: [0, 60000],
+    round: 1,
+    easing: 'linear',
+    duration: 1555
+});
+
+anime({
+    targets: '#fb-count',
+    textContent: [0, 45531],
+    round: 1,
+    easing: 'linear',
+    duration: 1250
+});
+
+anime({
+    targets: '#ig-count',
+    textContent: [0, 10000],
+    round: 1,
+    easing: 'linear',
+    duration: 1000
+});
